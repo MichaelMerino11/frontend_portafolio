97cf635a76fd060c3d094fd057fea6c3fc2b244f
MaikiMerino11 <maikijunior9@gmail.com>
2025-03-10 18:08:20 -0500

    Subiendo solo el frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import styles from "../styles/Header.module.scss";
+import logo from "../assets/images/logo.png"; // Importamos el logo
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  // Cambiar el título de la pestaña
+  useEffect(() => {
+    document.title = "Portafolio Michael"; // Cambia el título de la pestaña
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -13,7 +19,11 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <h1 className={styles.logo}>Portafolio Profesional</h1>
+        {/* Logo y título */}
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="Logo" className={styles.logo} />
+          <h1 className={styles.title}>Portafolio Profesional</h1>
+        </div>
 
         {/* Botón de menú hamburguesa en móviles */}
         <button className={styles.menuButton} onClick={toggleMenu}>
