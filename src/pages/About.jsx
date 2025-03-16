45ec68d36331f1254766e63380d592ca4bacab7d
MaikiMerino11 <maikijunior9@gmail.com>
2025-03-15 20:30:10 -0500

    Test de animaciones completo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import About from "../components/About";
 import Layout from "../components/Layout";
 
+// Componente del button innecesario
+
 const AboutPage = () => {
   return (
     <Layout>
